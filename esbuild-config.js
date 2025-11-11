c588e18d15e540383b60ae4bfdc4b59cdb510c5e
Mell Peçanha <mellpecanha@gmail.com>
2025-11-10 22:26:12 -0300

    feat: initialize library management API w/ TS, prisma

---
+++ esbuild-config.js
@@ -0,0 +1,34 @@
+import esbuild from 'esbuild';
+
+const config = {
+  entryPoints: ['src/index.ts'],
+  bundle: true,
+  outfile: 'dist/index.js',
+  tsconfig: 'tsconfig.json',
+  platform: 'node',
+  sourcemap: true,
+  format: "esm",
+  external: [
+    'events',
+    // '@prisma/client',
+    // '@prisma/adapter-better-sqlite3',
+    // 'better-sqlite3',
+    // '.prisma/client',
+
+    // 'node:path',
+    // 'node:url',
+    // 'node:fs',
+    // 'node:os'
+  ],
+};
+
+const main = async () => {
+  if (process.argv.includes('--watch')) {
+    const ctx = await esbuild.context(config);
+    await ctx.watch();
+  }
+
+  await esbuild.build(config)
+}
+
+main();
